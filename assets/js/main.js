ee4158b2e2094e5d9c0a548702d0ea0b3e1fbd80
Fernando Ossiel Cruz Medina <ossielfer.cruz@gmail.com>
2026-02-18 21:00:17 -0600

    Creación base JS con selectores de vista

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const listView = document.getElementById("listView");
+  const calendarView = document.getElementById("calendarView");
+  const viewListBtn = document.getElementById("viewListBtn");
+  const viewCalendarBtn = document.getElementById("viewCalendarBtn");
+
+  if (!listView || !calendarView || !viewListBtn || !viewCalendarBtn) {
+    return;
+  }
+});
