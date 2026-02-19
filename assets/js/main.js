189562eb6743c53695753de1a75e40e12fa69dd2
Fernando Ossiel Cruz Medina <ossielfer.cruz@gmail.com>
2026-02-18 21:36:35 -0600

    Agregando lista de materias, dias y horas para el calendario

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,29 @@ document.addEventListener("DOMContentLoaded", () => {
     return;
   }
 
+  const entries = [
+    { code: "0413", subject: "PROGRAMACION WEB", group: "Gpo1", day: "MARTES", time: "10:00 - 11:40", room: "E201" },
+    { code: "0413", subject: "PROGRAMACION WEB", group: "Gpo1", day: "JUEVES", time: "15:00 - 16:40", room: "E201" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo3", day: "LUNES", time: "08:00 - 09:40", room: "D104" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo3", day: "MIERCOLES", time: "08:00 - 09:40", room: "D104" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo4", day: "LUNES", time: "10:00 - 11:40", room: "E201" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo4", day: "JUEVES", time: "08:00 - 09:40", room: "E201" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo7", day: "LUNES", time: "03:00 - 04:40", room: "D104" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo7", day: "JUEVES", time: "01:00 - 02:40", room: "D104" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo8", day: "MARTES", time: "03:00 - 04:40", room: "D104" },
+    { code: "0402", subject: "INTRO. INGENIERIA", group: "Gpo8", day: "MIERCOLES", time: "01:00 - 02:40", room: "D104" },
+  ];
+
+  const days = ["LUNES", "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO"];
+  const timeOrder = [
+    "08:00 - 09:40",
+    "10:00 - 11:40",
+    "01:00 - 02:40",
+    "03:00 - 04:40",
+    "15:00 - 16:40",
+  ];
+  const timeSlots = timeOrder.filter((time) => entries.some((entry) => entry.time === time));
+
   const setActive = (isList) => {
     listView.classList.toggle("d-none", !isList);
     calendarView.classList.toggle("d-none", isList);
